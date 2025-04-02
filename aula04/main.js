c0261a7ce60b27adc9d555446f7b7f408ed85736
Patric Taborda <patricmorales1983@gmail.com>
2025-04-01 21:52:49 -0300

    TA AE

--- aula04/main.js
+++ aula04/main.js
@@ -1,1 +1,35 @@
 //CÓDIGO
+
+let listaProdutos = [];
+let autoIncrement = 1;
+
+function lister(){
+    return listaProdutos;
+}
+
+function inserir(produto){
+    produto.id = autoIncrement++;
+    listaProdutos.push(produto);
+}
+
+buscarIndicePorId(id){
+    listaProdutos.findIndex( (produto) => produto.id === id );
+}
+
+function atualizar(id, produtoAtual){
+    let (indice >= 0){
+        produtoAtual.id = id;
+        listaProdutos[indice] = produtoAtual;
+    }
+}
+
+main() {
+    inserir({nome:"Produto 1", preco 30});
+    inserir({nome:"Produto 2", preco 40});
+    inserir({nome:"Produto 3", preco 50});
+
+    console.log(listar());
+
+    atualizar(2, (id:2, nome:"Produto 2", preco: 45));
+    console.log(listar());
+}
